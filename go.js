2ded90bd362b132f8e45a62725eabb0470a74615
nursbayke <torokulovnursultan457@gmail.com>
2023-04-25 09:23:33 +0600

    second commit

---
+++ go.js
@@ -0,0 +1,38 @@
+// // let  userAdmin = prompt ('Введите имя пользователя');
+// // if(userAdmin == 'Admin') {
+// //   let question = prompt('Введите пароль');
+// //   let(question == 'admin123');{
+// //     alert('Привет Админ')
+// //   }
+// // }
+// //  else if (userAdmin == 'Пользователь') {
+// //     alert('Добро пожаловать,пользователь');
+// // }
+// // // console.log(' Введите номер операции: 1.Сложение 2.Вычитание 3.Умножение');
+
+// // const operation= parseInt(prompt("Введите номер операции от 1 до 3"));
+// // switch (operation)
+// // {
+// //         case 1:
+// //         console.log("Сложение");
+// //         alert("Сложение")
+// //         break;
+// //         case 2:
+// //         console.log("Вычитание");
+// //         alert("Вычитание");
+// //         break;
+// //         case 3:
+// //         console.log("Умножение");
+// //         alert("Умножение");
+// //         break;
+// // }
+// // console.log(alert(operation));
+// const arr = []
+// function check(obj) {
+//     if(obj.length) {
+//         alert(' arr не  пустой ');
+//     } else {
+//         alert('arr пустой')
+//     }
+// }
+//  check(arr);
